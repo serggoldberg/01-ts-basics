efb4ba6c5fb17e832f289a8cbb6b7f3a64c6ec0e
Lina_Goldberg <lina.goldberg5@gmail.com>
2025-12-09 23:54:51 +0100

    add task1 and task2

---
+++ src/task-1.ts
@@ -0,0 +1,9 @@
+const user: { name: string; age: number; isOnline: boolean } = {
+  name: "Alice",
+  age: 30,
+  isOnline: true,
+};
+
+console.log(`Name: ${user.name}`);
+console.log(`Age: ${user.age}`);
+console.log(`Online: ${user.isOnline}`);
